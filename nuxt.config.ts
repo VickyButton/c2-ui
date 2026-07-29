653d98ef103cb5f1cb2e5c21192881d4dacf6ffa
VickyButton <vickythewebdev@gmail.com>
2026-07-29 18:59:40 -0500

    feature: Use main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: {
-    enabled: true, 
+    enabled: true,
   },
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
 });
