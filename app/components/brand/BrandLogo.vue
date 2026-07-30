dd1e300006d7d823b33184bf5c41873757d82d76
VickyButton <vickythewebdev@gmail.com>
2026-07-29 20:41:22 -0500

    feature: Add brand logo component

---
+++ app/components/brand/BrandLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center gap-x-2 text-white">
+    <BaseIcon size="md">
+      <img src="~/assets/icons/drone.png" alt="drone with a camera" />
+    </BaseIcon>
+    <h1 class="text-4xl">C2-UI</h1>
+  </div>
+</template>
