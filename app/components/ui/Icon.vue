e32595ec038f18c03de58b4fbbf5c490184e9b40
VickyButton <vickythewebdev@gmail.com>
2026-07-29 20:40:26 -0500

    feature: Add icon component

---
+++ app/components/ui/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="{
+    'w-8': size === 'sm',
+    'w-12': size === 'md',
+    'w-16': size === 'lg',
+    }">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  size?: 'sm' | 'md' | 'lg';
+}>(), {
+  size: 'md',
+});
+</script>
