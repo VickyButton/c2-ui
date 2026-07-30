af700551e868a1570d0447e4356f545054000db4
VickyButton <vickythewebdev@gmail.com>
2026-07-29 21:43:13 -0500

    feature: Update card background color

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col border border-primary-500 text-white">
+  <div class="flex flex-col bg-secondary-600 border border-primary-500 text-white">
     <slot />
   </div>  
 </template>
