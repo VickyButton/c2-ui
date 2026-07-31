9fa12b52209d8a9203e6228bfde667838ee56911
VickyButton <vickythewebdev@gmail.com>
2026-07-31 11:56:08 -0500

    feature: Add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col w-screen h-screen bg-secondary-700">
+    <main class="flex flex-col justify-center items-center gap-y-4 w-full h-full">
+      <BrandLogo />
+      <slot />
+    </main>
+  </div>
+</template>
