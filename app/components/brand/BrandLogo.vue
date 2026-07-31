e84f199727bd9088d10ea01f9f5cbcf3320b9b9f
vickybutton <vickythewebdev@gmail.com>
2026-07-31 12:48:54 -0500

    feature: Remove text from brand logo

--- app/components/brand/BrandLogo.vue
+++ app/components/brand/BrandLogo.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex items-center gap-x-2 text-white">
-    <BaseIcon size="md">
+    <BaseIcon size="lg">
       <img src="~/assets/icons/drone.png" alt="drone with a camera" />
     </BaseIcon>
-    <h1 class="text-4xl">C2-UI</h1>
   </div>
 </template>
