e9a49db8027c0e6ef199b30157973abddb861608
vickybutton <vickythewebdev@gmail.com>
2026-08-01 11:34:38 -0500

    refactor: Adjust text color definitions

--- app/components/brand/BrandLogo.vue
+++ app/components/brand/BrandLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center gap-x-2 text-white">
+  <div class="flex items-center gap-x-2">
     <BaseIcon size="md">
       <img src="~/assets/icons/drone.png" alt="drone with a camera" />
     </BaseIcon>
