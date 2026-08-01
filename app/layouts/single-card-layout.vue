1fccb57c205df2ce228ca775ad7b385b34701fc5
vickybutton <vickythewebdev@gmail.com>
2026-08-01 11:32:05 -0500

    refactor: Move background styling to CSS file

--- app/layouts/single-card-layout.vue
+++ app/layouts/single-card-layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-screen h-screen bg-secondary-700">
+  <div class="flex flex-col w-screen h-screen">
     <main class="flex flex-col justify-center items-center gap-y-4 w-full h-full">
       <BrandLogo />
       <slot />
