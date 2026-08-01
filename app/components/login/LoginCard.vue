60d369d2c356ce27840c882757a371d6c71fd86b
vickybutton <vickythewebdev@gmail.com>
2026-08-01 00:57:41 -0500

    feature: Add login event to LoginCard

--- app/components/login/LoginCard.vue
+++ app/components/login/LoginCard.vue
@@ -1,7 +1,35 @@
 <template>
   <BaseCard>
     <BaseCardContent>
-      <LoginForm />
+      <LoginForm @submit="onSubmit" />
     </BaseCardContent>
   </BaseCard>
 </template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'login', accessToken: string): void;
+}>();
+
+const props = defineProps<{
+  auth: (data: {
+    username: string;
+    password: string;
+  }) => Promise<{
+    accessToken: string;
+  }>;
+}>();
+
+async function onSubmit(data: {
+  username: string;
+  password: string;
+}) {
+  try {
+    const response = await props.auth(data);
+
+    emit('login', response.accessToken);
+  } catch {
+    // TODO: Implement error handling.
+  }
+}
+</script>
