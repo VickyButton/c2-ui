e7072898e04ecc0b14b5b0dc2f72dc12da143670
vickybutton <vickythewebdev@gmail.com>
2026-07-31 22:13:07 -0500

    refactor: Use form components in login form

--- app/components/login/LoginCard.vue
+++ app/components/login/LoginCard.vue
@@ -1,9 +1,5 @@
 <template>
   <BaseCard>
-    <BaseCardHeader>
-      <BaseCardTitle>Login</BaseCardTitle>
-      <BaseCardDescription>Enter credentials below.</BaseCardDescription>
-    </BaseCardHeader>
     <BaseCardContent>
       <LoginForm />
     </BaseCardContent>
--- app/components/login/LoginForm.vue
+++ app/components/login/LoginForm.vue
@@ -1,7 +1,26 @@
 <template>
   <BaseForm>
+    <BaseFormHeader>      
+      <BaseFormTitle>Login</BaseFormTitle>
+      <BaseFormDescription>Enter credentials below.</BaseFormDescription>
+    </BaseFormHeader>
+    <BaseFormFields>
+      <BaseField>
+        <BaseFieldLabel :for="INPUT_USERNAME_ID" autocomplete="username">Username</BaseFieldLabel>
+        <BaseInputText :id="INPUT_USERNAME_ID" :name="INPUT_USERNAME_ID" />
+      </BaseField>
+      <BaseField>
+        <BaseFieldLabel :for="INPUT_PASSWORD_ID" autocomplete="current-password">Password</BaseFieldLabel>
+        <BaseInputPassword :id="INPUT_PASSWORD_ID" :name="INPUT_PASSWORD_ID" />
+      </BaseField>
+    </BaseFormFields>
     <BaseFormActions>
       <BaseButtonPrimary>Login</BaseButtonPrimary>
     </BaseFormActions>
   </BaseForm>
 </template>
+
+<script setup lang="ts">
+const INPUT_USERNAME_ID = 'username';
+const INPUT_PASSWORD_ID = 'password';
+</script>
