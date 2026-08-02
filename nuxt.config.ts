68623f22b448e186c0ccbcd3ec7eaeb4cde234ea
vickybutton <vickythewebdev@gmail.com>
2026-08-02 01:17:17 -0500

    chore: Turn off Nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: {
-    enabled: true,
+    enabled: false,
   },
   css: ['~/assets/css/main.css'],
   components: [
