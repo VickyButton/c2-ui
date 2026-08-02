180c92b580bbf770bf5621b1261f9efee1f08c19
vickybutton <vickythewebdev@gmail.com>
2026-08-02 09:52:08 -0500

    feature: Restrict dev settings to dev environment

--- app/components/settings/SettingsView.vue
+++ app/components/settings/SettingsView.vue
@@ -1,7 +1,11 @@
 <template>
   <AppLayoutPage title="SETTINGS">
     <div class="flex flex-col w-full max-w-[400px]">
-      <SettingsFeatureFlagsCard />
+      <SettingsFeatureFlagsCard v-if="showDevSettings" />
     </div>
   </AppLayoutPage>
 </template>
+
+<script setup lang="ts">
+const showDevSettings = isDevEnvironment();
+</script>
