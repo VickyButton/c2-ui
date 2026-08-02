f969c0f38fe86b9bd46ee733efb3752cb69c6d1a
vickybutton <vickythewebdev@gmail.com>
2026-08-02 00:40:25 -0500

    feature: Hide WIP page nav links in sidebar

--- app/components/appLayout/sidebar/AppLayoutSidebar.vue
+++ app/components/appLayout/sidebar/AppLayoutSidebar.vue
@@ -6,17 +6,17 @@
           <img src="~/assets/icons/dashboard.png" />
         </AppLayoutSidebarItemIcon>
       </AppLayoutSidebarItem>
-      <AppLayoutSidebarItem to="/devices" label="DEVICES">
+      <AppLayoutSidebarItem v-if="featureFlags.showDevicesPage" to="/devices" label="DEVICES">
         <AppLayoutSidebarItemIcon>
           <img src="~/assets/icons/drone.png" />
         </AppLayoutSidebarItemIcon>
       </AppLayoutSidebarItem>
-      <AppLayoutSidebarItem to="/routes" label="ROUTES">
+      <AppLayoutSidebarItem v-if="featureFlags.showRoutesPage" to="/routes" label="ROUTES">
         <AppLayoutSidebarItemIcon>
           <img src="~/assets/icons/route.png" />
         </AppLayoutSidebarItemIcon>
       </AppLayoutSidebarItem>
-      <AppLayoutSidebarItem to="/missions" label="MISSIONS">
+      <AppLayoutSidebarItem v-if="featureFlags.showMissionsPage" to="/missions" label="MISSIONS">
         <AppLayoutSidebarItemIcon>
           <img src="~/assets/icons/target.png" />
         </AppLayoutSidebarItemIcon>
@@ -36,3 +36,7 @@
     </AppLayoutSidebarItemGroup>
   </nav>
 </template>
+
+<script setup lang="ts">
+const { featureFlags } = useFeatureFlags();
+</script>
