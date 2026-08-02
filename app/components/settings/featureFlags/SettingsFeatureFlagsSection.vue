a31964a6d9544f65717aa20bd9ca0e3eb9f729e3
vickybutton <vickythewebdev@gmail.com>
2026-08-02 01:14:44 -0500

    chore: Grow labels in feature flags toggle

--- app/components/settings/featureFlags/SettingsFeatureFlagsSection.vue
+++ app/components/settings/featureFlags/SettingsFeatureFlagsSection.vue
@@ -4,15 +4,15 @@
       <legend>Toggle feature flags:</legend>
       <div class="flex flex-row gap-x-2">
         <input id="showDevicesPage" type="checkbox" name="showDevicesPage" v-model="featureFlags.showDevicesPage" />
-        <label for="showDevicesPage">Show Devices page</label>
+        <label for="showDevicesPage" class="grow">Show Devices page</label>
       </div>
       <div class="flex flex-row gap-x-2">
         <input id="showMissionsPage" type="checkbox" name="showMissionsPage" v-model="featureFlags.showMissionsPage" />
-        <label for="showMissionsPage">Show Missions page</label>
+        <label for="showMissionsPage" class="grow">Show Missions page</label>
       </div>
       <div class="flex flex-row gap-x-2">
         <input id="showRoutesPage" type="checkbox" name="showRoutesPage" v-model="featureFlags.showRoutesPage" />
-        <label for="showRoutesPage">Show Routes page</label>
+        <label for="showRoutesPage" class="grow">Show Routes page</label>
       </div>
     </fieldset>
   </section>
