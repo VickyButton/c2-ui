80cf138e1931acf1b91158d1c35721c77bc776aa
vickybutton <vickythewebdev@gmail.com>
2026-08-02 22:17:00 -0500

    feature: Scaffold badge component

---
+++ app/components/ui/badge/Badge.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="px-2 border" :class="{
+    'border-gray-300 text-gray-300 bg-gray-300/20': variant === 'neutral',
+    'border-green-500 text-green-500 bg-green-500/20': variant === 'success',
+    'border-yellow-500 text-yellow-500 bg-yellow-500/20': variant === 'warning',
+    'border-red-500 text-red-500 bg-red-500/20': variant === 'danger',
+  }">{{ label }}</span>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  label: string;
+  variant: 'neutral' | 'success' | 'warning' | 'danger';
+}>(), {
+  variant: 'neutral',
+});
+</script>
