6a1c6b6ef0f8a745a08d7a81e27eb13251c0512e
vickybutton <vickythewebdev@gmail.com>
2026-08-03 15:00:38 -0500

    chore: Rename devices API SyncDevice to GetDevice

--- app/services/DevicesAPI.types.ts
+++ app/services/DevicesAPI.types.ts
@@ -1,4 +1,4 @@
 import type { Device } from '~/types/devices';
 
 export type DevicesAPI_GetDevices = () => Promise<Device[]>;
-export type DevicesAPI_SyncDevice = (deviceId: string) => Promise<Device>;
+export type DevicesAPI_GetDevice = (deviceId: string) => Promise<Device>;
