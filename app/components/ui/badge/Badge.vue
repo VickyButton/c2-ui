7e31688b6ee2ed1efe4d0a021c7109ebd08ef777
vickybutton <vickythewebdev@gmail.com>
2026-08-02 22:19:23 -0500

    fix: Center badge label

--- app/components/ui/badge/Badge.vue
+++ app/components/ui/badge/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="px-2 border" :class="{
+  <span class="flex items-center px-2 border" :class="{
     'border-gray-300 text-gray-300 bg-gray-300/20': variant === 'neutral',
     'border-green-500 text-green-500 bg-green-500/20': variant === 'success',
     'border-yellow-500 text-yellow-500 bg-yellow-500/20': variant === 'warning',
