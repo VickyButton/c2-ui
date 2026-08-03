a5aaa103e8ac0aa5507aef1a966426d9fec0c185
vickybutton <vickythewebdev@gmail.com>
2026-08-03 11:41:10 -0500

    feature: Create drone status badge component

---
+++ app/components/drone/DroneStatusBadge.vue
@@ -0,0 +1,36 @@
+<template>
+  <BaseBadge :label :variant />
+</template>
+
+<script setup lang="ts">
+import type { DroneStatus } from '~/types/devices';
+
+const props = defineProps<{
+  status: DroneStatus;
+}>();
+
+const label = computed(() => {
+  switch(props.status) {
+    case 'offline':
+      return 'OFFLINE';
+    case 'standby':
+      return 'STANDBY';
+    case 'active':
+      return 'ACTIVE';
+    default:
+      return 'ERROR';
+  }
+});
+const variant = computed(() => {
+  switch(props.status) {
+    case 'offline':
+      return 'neutral';
+    case 'standby':
+      return 'warning';
+    case 'active':
+      return 'success';
+    default:
+      return 'danger';
+  }
+})
+</script>
