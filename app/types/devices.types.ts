174905d222ebf5f315e3a371fbf598c89c8bb8c8
vickybutton <vickythewebdev@gmail.com>
2026-08-04 15:59:54 -0500

    feature: Add coordinates property to drones

--- app/types/devices.types.ts
+++ app/types/devices.types.ts
@@ -1,3 +1,5 @@
+import type { GlobalCoordinates } from './map.types';
+
 interface BaseDevice {
   id: string;
   nickname?: string;
@@ -6,7 +8,10 @@ interface BaseDevice {
 export type DroneStatus = 'offline' | 'standby' | 'active';
 export interface Drone extends BaseDevice {
   type: 'drone';
+  /** The status of the drone. */
   status: DroneStatus;
+  /** The last-recorded coordinates of the drone. */
+  lastCoordinates: GlobalCoordinates;
 }
 
 export type Device = Drone;
