225b6a1e2050725b9f1ebbaabe79a7b3f8a73d13
vickybutton <vickythewebdev@gmail.com>
2026-08-04 18:31:59 -0500

    fix: Allow usage of GPS coordinates in OpenLayers map

--- app/services/GlobalMapOL.ts
+++ app/services/GlobalMapOL.ts
@@ -2,11 +2,16 @@ import type { GlobalMap } from '~/types/map.types';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import { OSM } from 'ol/source';
+import { useGeographic } from 'ol/proj';
 
 export class GlobalMapOL implements GlobalMap {
   private _map?: Map;
 
   public load(containerId: string) {
+    // Allows GPS coordinates to be used.
+    useGeographic();
+
+    // Initialize map and mount to container.
     this._map = new Map({
       target: containerId,
       layers: [
