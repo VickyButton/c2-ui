75b1a43ae9a4c65e11a815ac68ebcd8f186160c9
vickybutton <vickythewebdev@gmail.com>
2026-08-03 23:37:56 -0500

    feature: Create DevicesGroup component

---
+++ app/components/devices/DevicesGroup.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it, vi } from 'vitest';
+import { mountSuspended } from '@nuxt/test-utils/runtime';
+import DroneInformationCard from '../drone/informationCard/DroneInformationCard.vue';
+import DevicesGroup from './DevicesGroup.vue';
+
+const getDevice = vi.fn();
+const getDevices = vi.fn();
+
+describe('DevicesGroup', () => {
+  it.each([
+    {
+      type: 'drone',
+      component: DroneInformationCard,
+    },
+  ])('presents $type devices using $component', async ({ type, component }) => {
+    getDevices.mockResolvedValueOnce([
+      {
+        id: '123',
+        type,
+      },
+    ]);
+
+    const wrapper = await mountSuspended(DevicesGroup, {
+      props: {
+        getDevice,
+        getDevices,
+      },
+    });
+
+    expect(wrapper.findComponent(component).exists()).toBe(true);
+  });
+});
---
+++ app/components/devices/DevicesGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="grid grid-cols-3 gap-4">
+    <p v-if="devices.length === 0">No devices registered.</p>
+    <li v-for="device in devices">
+      <DroneInformationCard v-if="device.type === 'drone'" :drone="device" @sync="() => syncDevice(device.id)" />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { DevicesAPI_GetDevice, DevicesAPI_GetDevices } from '~/services/DevicesAPI.types';
+import { useDevicesGroup } from '~/composables/devices/useDevicesGroup';
+
+const props = defineProps<{
+  getDevices: DevicesAPI_GetDevices;
+  getDevice: DevicesAPI_GetDevice;
+}>();
+
+const { devices, loadDevices, syncDevice } = useDevicesGroup({
+  getDevices: props.getDevices,
+  getDevice: props.getDevice,
+});
+
+onMounted(async () => {
+  await loadDevices();
+});
+</script>
