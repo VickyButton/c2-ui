eed282410f4cc4d08651ae2ef7b551dde80149f4
vickybutton <vickythewebdev@gmail.com>
2026-08-04 00:16:40 -0500

    fix: Make sidebar fixed width

--- app/components/appLayout/AppLayout.vue
+++ app/components/appLayout/AppLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col w-screen h-screen">
     <AppLayoutUtilityBar />
     <div class="flex flex-row grow">
-      <AppLayoutSidebar class="grow max-w-[200px] border-r" />
+      <AppLayoutSidebar class="shrink-0 w-[200px] border-r" />
       <main class="grow">
         <slot />
       </main>
