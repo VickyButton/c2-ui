6cf06601c56fc878245a51c49e0b0085afd89b71
vickybutton <vickythewebdev@gmail.com>
2026-08-04 00:17:05 -0500

    fix: Define heights for icons

--- app/components/ui/Icon.vue
+++ app/components/ui/Icon.vue
@@ -1,8 +1,8 @@
 <template>
   <div :class="{
-    'w-8': size === 'sm',
-    'w-12': size === 'md',
-    'w-16': size === 'lg',
+    'w-8 h-8': size === 'sm',
+    'w-12 h-12': size === 'md',
+    'w-16 h-16': size === 'lg',
     }">
     <slot />
   </div>
