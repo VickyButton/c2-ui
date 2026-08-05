b8a0f806352e7669816f35b28268b7be9722428e
vickybutton <vickythewebdev@gmail.com>
2026-08-05 18:15:05 -0500

    feature: Create DroneBattery component and associated unit test

---
+++ app/components/drone/DroneBattery.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from 'vitest';
+import { mountSuspended } from '@nuxt/test-utils/runtime';
+import Icon from '../ui/Icon.vue';
+import DroneBattery from './DroneBattery.vue';
+
+describe('DroneBattery', () => {
+  it.each([
+    {
+      battery: 75,
+      name: 'battery-full',
+    },
+    {
+      battery: 50,
+      name: 'battery-healthy',
+    },
+    {
+      battery: 25,
+      name: 'battery-half',
+    },
+    {
+      battery: 10,
+      name: 'battery-low',
+    },
+    {
+      battery: 0,
+      name: 'battery-empty',
+    },
+  ])('presents $name icon when battery is at $battery%', async ({ battery, name }) => {
+    const wrapper = await mountSuspended(DroneBattery, {
+      props: {
+        battery,
+      },
+    });
+
+    const icon = wrapper.getComponent(Icon);
+    expect(icon.props()).toEqual({
+      name,
+      size: 'md',
+    });
+  });
+});
---
+++ app/components/drone/DroneBattery.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseIcon :name :title />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  battery: number;
+}>();
+
+const title = computed(() => `${props.battery}%`)
+const name = computed(() => {
+  /*
+    Battery States:
+    - Full      battery >= 75%
+    - Healthy   battery >= 50%
+    - Half      battery >= 25%
+    - Low       battery >= 10%
+    - Empty     battery < 10%
+  */
+  if (props.battery >= 75) {
+    return 'battery-full';
+  } else if (props.battery >= 50) {
+    return 'battery-healthy';
+  } else if (props.battery >= 25) {
+    return 'battery-half';
+  } else if (props.battery >= 10) {
+    return 'battery-low';
+  } else {
+    return 'battery-empty';
+  }
+});
+</script>
