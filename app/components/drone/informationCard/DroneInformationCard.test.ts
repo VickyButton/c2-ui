21939f2b744339fd9c8f8c36b8dd218ed2923a42
vickybutton <vickythewebdev@gmail.com>
2026-08-05 17:47:09 -0500

    refactor: Use base object for drone props in unit test

--- app/components/drone/informationCard/DroneInformationCard.test.ts
+++ app/components/drone/informationCard/DroneInformationCard.test.ts
@@ -2,22 +2,25 @@ import { mountSuspended } from '@nuxt/test-utils/runtime';
 import { describe, expect, it } from 'vitest';
 import DroneInformationCard from './DroneInformationCard.vue';
 
+const drone = {
+  id: '123',
+  type: 'drone' as const,
+  status: 'active' as const,
+  coordinates: {
+    latitude: 0,
+    longitude: 0,
+    altitude: 0,
+  },
+};
+
 describe('DroneInformationCard', () => {
   it('presents nickname if passed', async () => {
-    const drone = {
-      id: '123',
-      nickname: 'some_nickname',
-      type: 'drone' as const,
-      status: 'active' as const,
-      coordinates: {
-        latitude: 0,
-        longitude: 0,
-        altitude: 0,
-      },
-    };
     const wrapper = await mountSuspended(DroneInformationCard, {
       props: {
-        drone,
+        drone: {
+          ...drone,
+          nickname: 'some_nickname',
+        },
       },
     });
 
@@ -25,19 +28,10 @@ describe('DroneInformationCard', () => {
   });
 
   it('presents drone ID if nickname is not passed', async () => {
-    const drone = {
-      id: '123',
-      type: 'drone' as const,
-      status: 'active' as const,
-      coordinates: {
-        latitude: 0,
-        longitude: 0,
-        altitude: 0,
-      },
-    };
     const wrapper = await mountSuspended(DroneInformationCard, {
       props: {
         drone,
+        nickname: undefined,
       },
     });
 
