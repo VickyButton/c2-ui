846a99aa82e14a6930bbb11b0373247f2e4c00b1
vickybutton <vickythewebdev@gmail.com>
2026-08-09 16:20:44 -0500

    refactor: Move v-if to section from ul

--- app/components/devices/DevicesGroup.vue
+++ app/components/devices/DevicesGroup.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col gap-y-4">
     <p v-if="devices.length === 0">No devices registered.</p>
-    <section class="flex flex-col gap-y-2">
+    <section v-if="drones.length > 0" class="flex flex-col gap-y-2">
       <h2>DRONES</h2>
-      <ul v-if="drones.length > 0" class="grid grid-cols-3 gap-4">
+      <ul class="grid grid-cols-3 gap-4">
         <li v-for="drone in drones">
           <DroneInformationCard :drone @sync="syncDevice(drone.id)" />
         </li>
