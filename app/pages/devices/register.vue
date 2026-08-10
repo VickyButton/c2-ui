e5977691e19e40a38ac9277037f992c1097ea142
vickybutton <vickythewebdev@gmail.com>
2026-08-10 01:55:50 -0500

    feature: Add page title and auth guard middleware to device registration page

--- app/pages/devices/register.vue
+++ app/pages/devices/register.vue
@@ -1,3 +1,13 @@
 <template>
   <RegisterDeviceView />
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Register Device',
+});
+
+definePageMeta({
+  middleware: ['auth-guard'],
+});
+</script>
