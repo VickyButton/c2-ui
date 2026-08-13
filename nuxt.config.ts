91cbff08fcbdeb49e9cb585b9741fb1c8514ba68
vickybutton <vickythewebdev@gmail.com>
2026-08-13 00:54:55 -0500

    feature: Add new environment variables for map tiles API URLs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBaseUrl: '',
+      mapTilesDefaultApiUrl: '',
+      mapTilesSatteliteApiUrl: '',
     },
   },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/test-utils/module'],
